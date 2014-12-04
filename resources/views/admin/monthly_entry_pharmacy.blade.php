96b6d9e67709a471109811baaf024a59d2fa3637
grass19900325 <grass19900325@gmail.com>
2014-12-04 18:04:58 +0200

    UI for admin monthly entry pharmacy

---
+++ resources/views/admin/monthly_entry_pharmacy.blade.php
@@ -0,0 +1,106 @@
+@extends('layouts.master')
+
+@section('top_header')
+  Monthly Entry Information
+@endsection
+
+
+@section('content')
+
+<!-- main area -->
+<div class="main-content">
+  <div class="row">
+    <div class="panel mb25">
+        <div class="panel-heading border">
+          Monthly Entry Information
+        </div>
+        <div class="panel-body">
+        <div class="table-responsive">
+        <table class="table table-bordered table-striped mb0">
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Date / Time</th>
+              <th>Company Name</th>
+              <th>Medicine Name</th>
+              <th>Piece</th>
+              <th>Price</th>
+              <th>Subtotal</th>
+            </tr>
+          </thead>
+          <tfoot>
+            <tr>
+              <th>No.</th>
+              <th>Date / Time</th>
+              <th>Company Name</th>
+              <th>Medicine Name</th>
+              <th>Piece</th>
+              <th>Price</th>
+              <th>Subtotal</th>
+            </tr>         
+            </tfoot>
+          <tbody>
+            <tr>
+              <td>01</td>     
+              <td>13-Oct-2016</td>    
+              <td>Square</td>         
+              <td>Neoceptin R</td>         
+              <td>500</td>
+              <td>2 Tk.</td>
+              <td>1000 Tk.</td>                 
+            </tr>
+            <tr>
+              <td>01</td>     
+              <td>13-Oct-2016</td>    
+              <td>Square</td>         
+              <td>Neoceptin R</td>         
+              <td>500</td>
+              <td>2 Tk.</td>
+              <td>1000 Tk.</td>                 
+            </tr><tr>
+              <td>01</td>     
+              <td>13-Oct-2016</td>    
+              <td>Square</td>         
+              <td>Neoceptin R</td>         
+              <td>500</td>
+              <td>2 Tk.</td>
+              <td>1000 Tk.</td>                 
+            </tr><tr>
+              <td>01</td>     
+              <td>13-Oct-2016</td>    
+              <td>Square</td>         
+              <td>Neoceptin R</td>         
+              <td>500</td>
+              <td>2 Tk.</td>
+              <td>1000 Tk.</td>                 
+            </tr><tr>
+              <td>01</td>     
+              <td>13-Oct-2016</td>    
+              <td>Square</td>         
+              <td>Neoceptin R</td>         
+              <td>500</td>
+              <td>2 Tk.</td>
+              <td>1000 Tk.</td>                 
+            </tr>
+            <tr>
+              <td></td>         
+              <td></td>         
+              <td></td>         
+              <td></td>
+              <td></td>
+              <td></td>
+              <td>2,75,000 Tk.</td>                 
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      </div>
+    </div>
+  </div>
+  <!--Admit Patient Section End-->
+
+  <!-- /main area -->
+</div>
+<!-- /content panel -->
+@endsection
